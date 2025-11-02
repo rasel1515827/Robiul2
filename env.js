94e382bdf56a12b3b3e46ac3399a2f81d9e4210e
rasel1515827 <mdrobiulfdsell@gmail.com>
2025-11-02 15:33:10 +0600

    Create env.js

---
+++ env.js
@@ -0,0 +1,18 @@
+// env.js
+// This file contains all your sensitive information.
+
+export const USERNAME = "hjsadty19@gmail.com";
+export const PASSWORD = "Mahfuz2007#";
+
+export const BOT_TOKEN = "7817132513:AAGv0l-sGkDAP3zzB0f3QaL2bQyunXL51cE";
+export const CHAT_ID = "-1002845079730";
+
+export const REFRESH_INTERVAL_MINUTES = 30;
+export const AUDIO_TRANSCRIPTION_RETRIES = 1;
+
+
+export const MAIN_CHANNEL_NAME = "Main Channel";
+export const MAIN_CHANNEL_URL = "https://t.me/+IBcsqhCNluk5YTA1";
+
+export const ADMIN_NAME = "Contact Admin";
+export const ADMIN_URL = "https://t.me/BD_MAHFUZ_UP";
